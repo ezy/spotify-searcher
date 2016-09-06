e8b6bf4de0293d684267639b47edd149e25c61cf
Ezra Keddell <ezrakeddell@gmail.com>
2016-09-07 08:29:04 +1200

    fix handlebars syntax

--- app/index/controller.js
+++ app/index/controller.js
@@ -7,4 +7,12 @@ export default Ember.Controller.extend({
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
 
+  actions: {
+    saveInvitation() {
+      alert(`Saving email address: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  },
+
 });

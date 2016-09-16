c3bd16a434379350ea23edb2688623507694a6b4
Ezra Keddell <ezrakeddell@gmail.com>
2016-09-17 10:30:00 +1200

    added contact mailer to db [Create an Admin page ]

--- app/contact/controller.js
+++ app/contact/controller.js
@@ -4,20 +4,27 @@ export default Ember.Controller.extend({
 
   message: '',
 
+  isEmail: Ember.computed.match('email', /^.+@.+\..+$/),
   isNotEmpty: Ember.computed.notEmpty('message'),
   correctMsgLength: Ember.computed.gte('message.length', 5),
-  isValid: Ember.computed.and('isNotEmpty', 'correctMsgLength'),
+  isValid: Ember.computed.and('isNotEmpty', 'correctMsgLength', 'isEmail'),
 
   actions: {
     sendMessage: function()  {
+
       let email = this.get('emailAddress');
       let message = this.get('message');
 
-      let responseMessage = 'To: ' + email + ', Message: ' + message;
+      const newContact = this.store.createRecord('contact', {
+        email: email,
+        message: message
+      });
 
-      this.set('responseMessage', responseMessage);
-      this.set('emailAddress', '');
-      this.set('message', '');
+      newContact.save().then((response) => {
+        this.set('responseMessage', response);
+        this.set('emailAddress', '');
+        this.set('message', '');
+      });
     }
   }
 

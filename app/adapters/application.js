cb9441c80831638dc5ab9370ebb76c66c5e61dee
Ezra Keddell <ezrakeddell@gmail.com>
2016-11-12 13:35:11 +1300

    update artist display

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,6 @@
-// import FirebaseAdapter from 'emberfire/adapters/firebase';
 import DS from  'ember-data';
 
 export default  DS.RESTfulAdapter.extend({
   host: "https://api.spotify.com",
   namespace: "v1"
 });
-
-// export default FirebaseAdapter.extend({
-// });
